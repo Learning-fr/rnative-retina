43a1fceb2d1f6854de7be7f17016fdd3b9ba0caa
frivolta <rivoltafilippo@gmail.com>
2019-07-09 10:18:33 +0200

    Fixing android studio error, starting redux

--- App.js
+++ App.js
@@ -5,60 +5,69 @@ import { ScrollView, SafeAreaView } from "react-native";
 import styled from "styled-components";
 import Card from "./components/Card";
 import { Icon } from "expo";
-import Logo from "./components/Logo";
 import { NotificationIcon } from "./components/Icons";
+import Logo from "./components/Logo";
 import Course from "./components/Course";
+import Menu from "./components/Menu";
 
-export default function App() {
-  return (
-    <Container>
-      <SafeAreaView>
-        <ScrollView>
-          <TitleBar>
-            <Avatar source={require("./assets/avatar.jpg")} />
-            <Title>Welcome back, </Title>
-            <Name>Meng</Name>
-            <NotificationIcon style={{ position: "absolute", right: 20, top: 5 }} />
-          </TitleBar>
-          <ScrollView
-            style={{ flexDirection: "row", padding: 20, paddingTop: 30, paddingLeft: 12 }}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-          >
-            {logos.map((logo, index) => (
-              <Logo image={logo.image} text={logo.text} key={index} />
-            ))}
-          </ScrollView>
-          <Subtitle>Continue Learning</Subtitle>
-          <ScrollView horizontal={true} style={{ paddingBottom: 30 }} showsHorizontalScrollIndicator={false}>
-            {cards.map((card, index) => (
-              <Card
+export default class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Menu />
+        <SafeAreaView>
+          <ScrollView style={{ height: "100%" }}>
+            <TitleBar>
+              <Avatar source={require("./assets/avatar.jpg")} />
+              <Title>Welcome back,</Title>
+              <Name>Meng</Name>
+              <NotificationIcon style={{ position: "absolute", right: 20, top: 5 }} />
+            </TitleBar>
+            <ScrollView
+              style={{
+                flexDirection: "row",
+                padding: 20,
+                paddingLeft: 12,
+                paddingTop: 30
+              }}
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+            >
+              {logos.map((logo, index) => (
+                <Logo key={index} image={logo.image} text={logo.text} />
+              ))}
+            </ScrollView>
+            <Subtitle>Continue Learning</Subtitle>
+            <ScrollView horizontal={true} style={{ paddingBottom: 30 }} showsHorizontalScrollIndicator={false}>
+              {cards.map((card, index) => (
+                <Card
+                  key={index}
+                  title={card.title}
+                  image={card.image}
+                  caption={card.caption}
+                  logo={card.logo}
+                  subtitle={card.subtitle}
+                />
+              ))}
+            </ScrollView>
+            <Subtitle>Popular Courses</Subtitle>
+            {courses.map((course, index) => (
+              <Course
                 key={index}
-                title={card.title}
-                image={card.image}
-                caption={card.caption}
-                logo={card.logo}
-                subtitle={card.subtitle}
+                image={course.image}
+                title={course.title}
+                subtitle={course.subtitle}
+                logo={course.logo}
+                author={course.author}
+                avatar={course.avatar}
+                caption={course.caption}
               />
             ))}
           </ScrollView>
-          <Subtitle>Popular courses</Subtitle>
-          {courses.map((course, index) => (
-            <Course
-              key={index}
-              image={course.image}
-              title={course.title}
-              subtitle={course.subtitle}
-              logo={course.logo}
-              author={course.author}
-              avatar={course.avatar}
-              caption={course.caption}
-            />
-          ))}
-        </ScrollView>
-      </SafeAreaView>
-    </Container>
-  );
+        </SafeAreaView>
+      </Container>
+    );
+  }
 }
 
 const Subtitle = styled.Text`
@@ -66,8 +75,19 @@ const Subtitle = styled.Text`
   font-weight: 600;
   font-size: 15px;
   margin-left: 20px;
-  margin-top: 20px;
+  margin-top: 10px;
   text-transform: uppercase;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
 `;
 
 const Container = styled.View`
@@ -88,20 +108,9 @@ const Name = styled.Text`
 `;
 
 const TitleBar = styled.View`
-width: 100%
-margin-top: 50px;
-padding-left: 80px;
-`;
-
-const Avatar = styled.Image`
-  width: 44px;
-  height: 44px;
-  background: black;
-  border-radius: 22px;
-  margin-left: 20px;
-  position: absolute;
-  top: 0;
-  left: 0;
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
 `;
 
 const logos = [
@@ -170,7 +179,7 @@ const courses = [
     logo: require("./assets/logo-studio.png"),
     author: "Meng To",
     avatar: require("./assets/avatar.jpg"),
-    caption: "Design an interactive prototype"
+    caption: "Design and interactive prototype"
   },
   {
     title: "React for Designers",
